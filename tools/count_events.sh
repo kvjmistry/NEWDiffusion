694cf63b0c2f5146db2482f49ebb7d0c9e1f4a80
Krishan Mistry <kvjmistry@gmail.com>
2022-04-13 15:37:29 -0500

    Add documentation to count events script

--- tools/count_events.sh
+++ tools/count_events.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 # Sums the number events stored in the count event files
+# The files input must be a file with a single number corresponding to the
+# number of events that were simulated
+
+# USAGE: source count_events.sh <file wildcard>
 
 sum=0
 for i in $*; do sum=$(($sum + $(cat $i))); done
